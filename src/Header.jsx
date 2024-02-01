d7ff6c9ed42e2c4ecb2294888015377a5785bf5b
Gotur Shrinivasa <gotur.shrinivasa@bridgestone.com>
2024-02-01 18:43:37 +0530

    Declaring Routes and implementing client side navigation

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,11 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+//Link is like anchor tag but react-router handles the click so the page doesn't reload
+//Navlink allows custom styling when the link is active
+
+const activeStyle = {
+  color: "green",
+};
 
 export default function Header() {
   return (
@@ -6,7 +13,19 @@ export default function Header() {
       <nav>
         <ul>
           <li>
-            <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            <Link to="/">
+              <img alt="Carved Rock Fitness" src="/images/logo.png" />
+            </Link>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to="/shoes">
+              Shoes
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={activeStyle} to="/Cart">
+              Cart
+            </NavLink>
           </li>
         </ul>
       </nav>
