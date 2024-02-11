2ee17420b5489f2a1115b6dc29fe71f31d2076f8
Gotur Shrinivasa <gotur.shrinivasa@bridgestone.com>
2024-02-11 11:19:12 +0530

    Filter by size option moved from Products page to shoe page

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom"; //this is help us to read parameters from the url
 import useFetch from "./useFetch";
 import Spinner from "./Spinner";
@@ -7,8 +7,9 @@ import PageNotFound from "./PageNotFound";
 export default function Detail() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const [sku, setSku] = useState("");
   const { data: product, loading, error } = useFetch(`products/${id}`);
-
+  console.log(product);
   if (loading) return <Spinner></Spinner>;
   if (product.length === 0) return <PageNotFound></PageNotFound>; //render pageNotFound component when there are no product
   if (error) throw error;
@@ -18,6 +19,16 @@ export default function Detail() {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+
+      <select id="size" value={sku} onChange={(e) => setSku(e.target.value)}>
+        <option value="">What Size?</option>
+        {product.skus.map((s) => (
+          <option key={s.sku} value={s.sku}>
+            {s.size}
+          </option>
+        ))}
+      </select>
+
       <p>
         <button
           className="btn btn-primary"
