853c122c6c03d6843d0e21759aeded2a44468ccc
Gotur Shrinivasa <gotur.shrinivasa@bridgestone.com>
2024-02-19 20:07:03 +0530

    Using Ref on Uncontrolled Component

---
+++ src/DetailRefs.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from "react";
+import { useParams, useNavigate } from "react-router-dom"; //this is help us to read parameters from the url
+import useFetch from "./useFetch";
+import Spinner from "./Spinner";
+import PageNotFound from "./PageNotFound";
+
+export default function Detail(props) {
+  const { id } = useParams();
+  const skuRef = useRef();
+  const navigate = useNavigate();
+  const { data: product, loading, error } = useFetch(`products/${id}`);
+  console.log(product);
+  if (loading) return <Spinner></Spinner>;
+  if (product.length === 0) return <PageNotFound></PageNotFound>; //render pageNotFound component when there are no product
+  if (error) throw error;
+
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+
+      <select id="size" ref={skuRef}>
+        <option value="">What Size?</option>
+        {product.skus.map((s) => (
+          <option key={s.sku} value={s.sku}>
+            {s.size}
+          </option>
+        ))}
+      </select>
+
+      <p>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            const sku = skuRef.current.value;
+            if (!sku) alert("Select the Size!");
+            props.addToCart(id, sku);
+            navigate("/cart");
+          }}
+        >
+          Add to Cart
+        </button>
+      </p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
+}
